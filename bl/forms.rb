6a333eb6280af2295f9549900fdc27020b8ad433
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-03-29 00:52:23 +0300

    forms

--- bl/forms.rb
+++ bl/forms.rb
@@ -3,7 +3,8 @@ $forms_posted = $mongo.collection('forms_posted')
 POST_FORM_WHITE_FIELDS = [:which_form,:name,:email,:text]
 
 post '/post_form' do
-  data = params.just(POST_FORM_WHITE_FIELDS)
+  data  = params
+  #data = params.just(POST_FORM_WHITE_FIELDS)
   $forms_posted.add(data)
   redirect '/refer'
 end
